486b47dd2e4bfc03c6c2f20fc0c889eda9fedac0
dreamsky2020 <59742254+dreamsky2020@users.noreply.github.com>
2022-03-19 05:36:55 +0800

    Add validation for login screen

--- src/components/Login/index.js
+++ src/components/Login/index.js
@@ -1,9 +1,10 @@
-import React, { useState, useRef } from 'react'
-import { View } from 'react-native'
+import React, { useState, useRef, useEffect } from 'react'
+import { View, Keyboard } from 'react-native'
 import { HButton, HText, HSliderButton, HPressableText } from '../Shared'
 import { Box, Input, FormControl, Icon, IconButton } from 'native-base'
 import MaterialIcons from 'react-native-vector-icons/MaterialIcons'
 import { colors } from '../../utils/styleGuide'
+import { useForm, Controller } from 'react-hook-form'
 
 import styles from './style'
 
@@ -13,8 +14,18 @@ export const Login = (props) => {
     onNavigationRedirect
   } = props
 
+  const { control, handleSubmit, formState: { errors, isValid } } = useForm()
   const [passwordSee, setPasswordSee] = useState(false)
   const inputRef = useRef()
+
+  const onSubmit = (values) => {
+    Keyboard.dismiss()
+    console.log(values)
+  }
+
+  const handleChangeInputEmail = (value, onChange) => {
+    onChange(value.toLowerCase().replace(/[&,()%";:ç?<>{}\\[\]\s]/g, ''))
+  }
 
   return (
     <View style={styles.wrapper}>
@@ -46,34 +57,47 @@ export const Login = (props) => {
       <Box mt={8}>
         <FormControl
           mb={4}
+          isInvalid={errors?.email?.message}
         >
           <FormControl.Label _text={styles.label} mb={1}>Email</FormControl.Label>
-          <Input
-            placeholder='Enter your email or username'
-            placeholderTextColor={colors.text03}
-            keyboardType="email-address"
-            fontSize={14}
-            borderRadius={8}
-            height={50}
-            borderColor={colors.borderColor}
-            autoCapitalize='none'
-            autoCorrect={false}
-            autoCompleteType='email'
-            returnKeyType='next'
-            onSubmitEditing={() => inputRef.current?.focus()}
-            blurOnSubmit={false}
-            InputLeftElement={
-              <Icon
-                as={<MaterialIcons name="email" />}
-                size={5} ml="4"
-                color={colors.text04}
+          <Controller
+            name='email'
+            control={control}
+            render={({ field: { onChange, value } }) => (
+              <Input
+                placeholder='Enter your email or username'
+                placeholderTextColor={colors.text03}
+                keyboardType="email-address"
+                fontSize={14}
+                borderRadius={8}
+                height={50}
+                borderColor={colors.borderColor}
+                autoCapitalize='none'
+                autoCorrect={false}
+                autoCompleteType='email'
+                returnKeyType='next'
+                value={value}
+                onChangeText={val => handleChangeInputEmail(val, onChange)}
+                onSubmitEditing={() => inputRef.current?.focus()}
+                blurOnSubmit={false}
+                InputLeftElement={
+                  <Icon
+                    as={<MaterialIcons name="email" />}
+                    size={5} ml="4"
+                    color={colors.text04}
+                  />
+                }
+                _focus={{
+                  borderColor: !errors?.email?.message ? colors.primary : colors.error
+                }}
               />
-            }
-            _invalid={{
-              borderColor: colors.error
-            }}
-            _focus={{
-              borderColor: colors.primary
+            )}
+            rules={{
+              required: { value: true, message: 'The field Email is required' },
+              pattern: {
+                value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+                message: 'Invalid email'
+              }
             }}
           />
           <FormControl.ErrorMessage
@@ -83,48 +107,54 @@ export const Login = (props) => {
             mt={1}
             _text={styles.errorText}
           >
-            Invalid email 
+            {errors?.email?.message}
           </FormControl.ErrorMessage>
         </FormControl>
 
-        <FormControl>
-          <FormControl.Label
-            _text={styles.label}
-            mb={1}
-          >Password</FormControl.Label>
-          <Input
-            placeholder='Enter your password'
-            type={passwordSee ? "text" : "password"}
-            fontSize={14}
-            borderRadius={8}
-            height={50}
-            borderColor={colors.borderColor}
-            placeholderTextColor={colors.text03}
-            autoCompleteType='password'
-            ref={inputRef}
-            returnKeyType='done'
-            blurOnSubmit
-            InputLeftElement={
-              <Icon
-                as={<MaterialIcons name="lock" />}
-                size={5} ml="4"
-                color={colors.text04}
+        <FormControl
+          isInvalid={errors?.password?.message}
+        >
+          <FormControl.Label _text={styles.label} mb={1}>Password</FormControl.Label>
+          <Controller
+            name='password'
+            rules={{ required: { value: true, message: 'The field Password is required' } }}
+            control={control}
+            render={({ field: { onChange, value } }) => (
+              <Input
+                placeholder='Enter your password'
+                type={passwordSee ? "text" : "password"}
+                fontSize={14}
+                borderRadius={8}
+                height={50}
+                borderColor={colors.borderColor}
+                placeholderTextColor={colors.text03}
+                autoCompleteType='password'
+                ref={inputRef}
+                returnKeyType='done'
+                blurOnSubmit
+                value={value}
+                onChangeText={val => onChange(val)}
+                onSubmitEditing={handleSubmit(onSubmit)}
+                InputLeftElement={
+                  <Icon
+                    as={<MaterialIcons name="lock" />}
+                    size={5} ml="4"
+                    color={colors.text04}
+                  />
+                }
+                InputRightElement={
+                  <Icon
+                    as={<MaterialIcons name={passwordSee ? "visibility" : "visibility-off"} />}
+                    size={5} mr="4"
+                    color={colors.text04}
+                    onPress={() => setPasswordSee(!passwordSee)}
+                  />
+                }
+                _focus={{
+                  borderColor: !errors?.password?.message ? colors.primary :colors.error
+                }}
               />
-            }
-            InputRightElement={
-              <Icon
-                as={<MaterialIcons name={passwordSee ? "visibility" : "visibility-off"} />}
-                size={5} mr="4"
-                color={colors.text04}
-                onPress={() => setPasswordSee(!passwordSee)}
-              />
-            }
-            _invalid={{
-              borderColor: colors.error
-            }}
-            _focus={{
-              borderColor: colors.primary
-            }}
+            )}
           />
           <FormControl.ErrorMessage
             leftIcon={
@@ -133,9 +163,15 @@ export const Login = (props) => {
             mt={1}
             _text={styles.errorText}
           >
-            Invalid password
+            {errors?.password?.message}
           </FormControl.ErrorMessage>
         </FormControl>
+        <Box alignItems='center' mt={8}>
+          <HButton
+            text='Log in'
+            onPress={handleSubmit(onSubmit)}
+          />
+        </Box>
       </Box>
       
       <View style={styles.forgotLink}>
@@ -144,11 +180,6 @@ export const Login = (props) => {
           onPress={() => {}}
         />
       </View>
-      <Box alignItems='center' mt={8}>
-        <HButton
-          text='Log in'
-        />
-      </Box>
     </View>
   )
 }
