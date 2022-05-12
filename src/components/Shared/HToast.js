5268364fb141da36591632676b883acb1d67b648
dreamsky2020 <92911264+devMichael0114@users.noreply.github.com>
2022-05-12 17:02:22 +0800

    [update] toast top

--- src/components/Shared/HToast.js
+++ src/components/Shared/HToast.js
@@ -20,7 +20,7 @@ const HToast = (props) => {
       paddingHorizontal: 32,
       paddingVertical: 25,
       borderRadius: 10,
-      top: insets.top ? -18 : -46
+      top: insets.top ? 0 : -46
     },
     messageText: {
       color: colors.white,
